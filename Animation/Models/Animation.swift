1179a201aac88acb0625dbe1886b38170f7d0c1a
Stanislav Sazonov <slavk11@yandex.ru>
2023-05-06 11:25:40 +0300

    remade model

--- Animation/Models/Animation.swift
+++ Animation/Models/Animation.swift
@@ -5,46 +5,31 @@
 //  Created by Сазонов Станислав on 01.05.2023.
 //
 
-
 struct Animation {
     let name: String
     let curve: String
     let force: Double
     let duration: Double
     let delay: Double
-    let buttonTitle: String
     
-    static func updateAnimation() -> [Animation] {
-        var randomAnimation: [Animation] = []
-        
-        let names = DataStore.shared.animationNames.shuffled()
-        let curves = DataStore.shared.curves.shuffled()
-        let forces = DataStore.shared.forces.shuffled()
-        let durations = DataStore.shared.durationTime.shuffled()
-        let delayTimes = DataStore.shared.delayTimes.shuffled()
-        let button = names[1]
-        
-        let iterationCount = min(
-            names.count,
-            curves.count,
-            forces.count,
-            durations.count,
-            delayTimes.count
+    var description: String {
+        """
+        preset: \(name)
+        curve: \(curve)
+        force: \(force)
+        duration: \(duration)
+        delay: \(delay)
+        """
+    }
+    
+    static var randomAnimation: Animation {
+        Animation(
+            name: DataStore.shared.animations.randomElement()?.rawValue ?? "wobble",
+            curve: DataStore.shared.animations.randomElement()?.rawValue ?? "easyIN",
+            force: Double.random(in: 1...1.5),
+            duration: Double.random(in: 0.8...1.6),
+            delay: 1
         )
-        
-        for index in 0..<iterationCount {
-            let animation = Animation(
-                name: names[index],
-                curve: curves[index],
-                force: forces[index],
-                duration: durations[index],
-                delay: delayTimes[index],
-                buttonTitle: button
-            )
-            randomAnimation.append(animation)
-            
-        }
-        return randomAnimation
     }
 }
 
